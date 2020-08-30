529f16f87b5bd2d6ebdbfa8fc9b0321ec234ce02
Dina07 <dine7952@gmail.com>
2020-08-30 11:42:05 +0530

    code reuse

--- app/src/main/java/com/urufit/aitum/activity/ScheduleActivity.java
+++ app/src/main/java/com/urufit/aitum/activity/ScheduleActivity.java
@@ -241,7 +241,6 @@ public class ScheduleActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_add_schedule, menu);
         return true;
     }
